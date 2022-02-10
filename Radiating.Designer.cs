6cad441efa8b8de401be3a18305383e63853ece1
Marcus Vinicius Thomas <TH@gsokoeln.onmicrosoft.com>
2022-02-10 21:39:22 +0100

    Neue Form angelegt.

---
+++ Radiating.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace gdiplusShowcase
+{
+    partial class Radiating
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Radiating
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 561);
+            this.MinimizeBox = false;
+            this.Name = "Radiating";
+            this.Text = "Radiating";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Radiating_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Radiating.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace gdiplusShowcase
+{
+    public partial class Radiating : Form
+    {
+        // System.Windows.Forms besitzt einen eigenen Timer, daher 
+        // muss die gewünschte Klasse hier explizit genannt werden.
+        System.Timers.Timer animationTimer = new System.Timers.Timer();
+
+        public Radiating()
+        {
+            InitializeComponent();
+            this.Size = new Size(600, 600);
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+            this.MinimumSize = this.Size;
+            this.MaximumSize = this.Size;
+        }
+
+        private void Radiating_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
